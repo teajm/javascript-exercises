a1f38b9392b0f44857325aabf5a8302563e7befc
Taylor Myrick <taylormyrick@gmail.com>
2022-12-21 21:31:49 -0600

    finished exercises 01/02/03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newStr = "";
+    if (num < 0){
+        return 'ERROR';
+    }
+    else{
+        for(let i = 0; i < num; i++){
+            newStr += string;
+        }
+    }
+    return newStr;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    newStr = "";
+    for(let i = string.length - 1; i >= 0; i--){
+        newStr += string[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
