7043fdb4c7fe3605dff6644cc944be239d76a3f7
Taylor Myrick <taylormyrick@gmail.com>
2022-12-21 23:26:06 -0600

    finished 05, added error checking and type checking

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (a > b){
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    if(typeof a != "number" || typeof b != "number" || a < 0 || b < 0){
+        return 'ERROR';
+    }
+    for(let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
