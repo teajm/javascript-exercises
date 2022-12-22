4c504b5f7efb5101db846534d147c27e338786b3
Taylor Myrick <taylormyrick@gmail.com>
2022-12-21 23:17:19 -0600

    finished 04 w/ testcases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
-};
-
+var removeFromArray = function(...args) {  
+    const nums = args[0];
+    // let newStr = [];
+    // for(const item of nums){
+    //     if(!args.includes(item)){
+    //         newStr.push(item);
+    //     }
+    // }
+    // return newStr;
+    //filter function filters out items from a string, in this case,
+    // it filters val such that val is not part of args.
+    return nums.filter(val => !args.includes(val));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
+function returnVal(val){
+    return !args.includes(val);
+}
