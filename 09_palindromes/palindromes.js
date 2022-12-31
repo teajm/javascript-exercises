888d66b0379a8474eaac665dfcf00c5df92a8288
Taylor Myrick <taylormyrick@gmail.com>
2022-12-30 21:12:14 -0600

    palindromes, finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    palindrome = palindrome.replace(/[^A-Za-z0-9]/g, '');
+    palindrome = palindrome.toLowerCase();
+    let splitString = palindrome.split("");
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join("");
+    return joinString === palindrome;
 };
 
 // Do not edit below this line
