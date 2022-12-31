a6c218b59c3eded6cd665fcb156f8834c944ab7e
Taylor Myrick <taylormyrick@gmail.com>
2022-12-30 21:22:16 -0600

    finished palindrome

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    let runningTotal = 1;
+    let prevNumber = 0;
+    let temp = 0;
+    if (n <= 0){
+        return "OOPS";
+    }
+    for (i = 1; i < n; i++){
+        temp = runningTotal;
+        runningTotal += prevNumber;
+        prevNumber = temp;
+    }
+    return runningTotal;
 };
 
 // Do not edit below this line
